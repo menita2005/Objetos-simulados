afde01fab4c62afb03881bed9980cd069af51820
Sebastian <menaloaizasebastian@gmail.com>
2024-03-14 04:34:59 -0500

    create 1-Manual(MockDatabase,TaxCalculator)

---
+++ SimulateObjectSolution/BLL.ClassLibrary.xUnit.Test/Manual/MockDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ClassLibrary.xUnit.Tests._1_Manual
+{
+    public class MockDatabase : IDatabase
+    {
+        public double GetTaxPercentageByPersonId(int personId)
+        {
+            switch (personId)
+            {
+                case 0:
+                    return 0.12;
+                case 1:
+                    return 0.27;
+                case 2:
+                    return 0.04;
+                default:
+                    return 0.18;
+            }
+        }
+    }
+}
---
+++ SimulateObjectSolution/BLL.ClassLibrary.xUnit.Test/Manual/TaxCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ClassLibrary.xUnit.Tests._1_Manual
+{
+    public class TaxCalculatorUnitTests
+    {
+        private readonly TaxCalculator _taxCalculator;
+        public TaxCalculatorUnitTests()
+        {
+            _taxCalculator = new TaxCalculator(new MockDatabase()); // Paso como parametro de un objeto anual simulado
+        }
+
+        [Theory]
+        [InlineData(112, 100, 0)]
+        [InlineData(127, 100, 1)]
+        [InlineData(104, 100, 2)]
+        [InlineData(118, 100, 6)]
+
+        public void CalculateTotalValueForPerson_SloudBeExpectedValue(double expected, double amount, int personId)
+        {
+            //Arrange
+
+            //Act
+            var resul = _taxCalculator.CalculateTotalValueForPerson(amount, personId);
+
+            //Assert
+            Assert.Equal(expected, resul);
+        }
+
+    }
+}
