6ee0fb925c220e6bd9bcd975cfbafd25c0789a34
Sebastian <menaloaizasebastian@gmail.com>
2024-03-14 04:28:28 -0500

    Create objet database and intarface

---
+++ SimulateObjectSolution/BLLClassLibrary/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ClassLibrary
+{
+    public class Database : IDatabase
+    {
+        public double GetTaxPercentageByPersonId(int personId)
+        {
+            //Todo el algoritmo para conectar a la DB y averiguar a partir del "PersonId" la tasa (%)
+            return 0;
+        }
+    }
+}
+
