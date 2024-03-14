a43081065ecec5e8c57c2a603eb13f66baf11eab
Sebastian <menaloaizasebastian@gmail.com>
2024-03-14 04:29:59 -0500

    add class TaxCalculator

---
+++ SimulateObjectSolution/BLLClassLibrary/TaxCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ClassLibrary
+{
+    public class TaxCalculator
+    {
+        private readonly IDatabase _database;
+
+        public TaxCalculator(IDatabase database)
+        {
+            this._database = database;
+        }
+
+        public double CalculateTotalValueForPerson(double amount, int id)
+        {
+            var tax = _database.GetTaxPercentageByPersonId(id);
+            return Math.Round(amount * (1 + tax), 2);
+        }
+    }
+}
